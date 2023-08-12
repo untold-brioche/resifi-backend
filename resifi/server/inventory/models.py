bd36c49cb363fc81f460fb5d1cf754ab659509c1
dogeystamp <dogeystamp@disroot.org>
2023-08-12 18:04:16 -0400

    resifi/server/inventory/models: added create date field

--- resifi/server/inventory/models.py
+++ resifi/server/inventory/models.py
@@ -1,4 +1,5 @@
 from resifi.server import db
+from datetime import datetime
 
 
 class InventoryItem(db.Model):
@@ -10,6 +11,11 @@ class InventoryItem(db.Model):
     charity_id = db.Column(db.String, db.ForeignKey("charity.id"))
     item_id = db.Column(db.String, db.ForeignKey("item.id"))
     item = db.relationship("Item", backref="inventory_item")
+    create_date = db.Column(db.DateTime, nullable=False)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.create_date = datetime.now()
 
     def to_dict(self):
         return {
@@ -20,5 +26,6 @@ class InventoryItem(db.Model):
             "business_id": self.business_id,
             "charity_id": self.charity_id,
             "item_id": self.item_id,
+            "create_date": self.create_date,
             "item": self.item.to_dict(),
         }
